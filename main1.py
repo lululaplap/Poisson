a4a614a2affc9dd06f77360f87922616f73c44f0
lululaplap <lewis@lappin.me.uk>
2019-04-04 22:12:53 +0100

    gfs

--- main1.py
+++ main1.py
@@ -4,21 +4,20 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import scipy.ndimage.filters as filter
 from mpl_toolkits.mplot3d import Axes3D
-from Jacobi import Jacobi as Poisson
+from Jacobi import Jacobi
 from GS_Poisson import GS
 fig = plt.figure()
 ax = fig.add_subplot(111)#,projection='3d')
-
+import sys
 
 def main():
-
     N = 30
     phi = np.random.uniform(-0.01,0.01,size=(N,N,N))
     rho = np.zeros((N,N,N))
     m = int(N/2)
-    rho[:,m,m] = 1
-    P = Poisson(N,phi,rho)
-    P = GS(N,phi,rho)
+    rho[m,m,m] = 1
+    PJ= Jacobi(N,phi,rho)
+    PGS = GS(N,phi,rho)
     #print(P.sim(0.0001))#,w=1.75))
     d = P.dist()
     P.sim(0.001)
