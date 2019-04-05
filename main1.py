9e710389f7cc505fe3fd4eddc90d8d7503452f8e
lululaplap <lewis@lappin.me.uk>
2019-04-05 15:55:40 +0100

    asdfghj

--- main1.py
+++ main1.py
@@ -42,13 +42,26 @@ def main(args):
         P.calcFeild()
         if str(args[2]) == 'E':
             plt.scatter(np.log(d[:,:,:].reshape(-1)),np.log(P.phi[:,:,:].reshape(-1)))
+            plt.title("Distance vs Phi")
+            plt.show()
+
+            E= np.sqrt(P.E[0]**2+P.E[1]**2+P.E[2]**2)#np.linalg.norm(self.B)
+            plt.scatter(np.log(d[:,:,:].reshape(-1)),np.log(E))
+            plt.title("Distance vs E")
             np.savetxt("EPhi.csv",P.phi[:,:,:].reshape(-1),delimiter=",")
+            np.savetxt("EF.csv",np.array(P.E).reshape(-1),delimiter=",")
         elif str(args[2]) == 'B':
             plt.scatter(np.log(d[m,:,:].reshape(-1)),np.log(P.phi[m,:,:].reshape(-1)))
+            plt.title("Distance vs A")
+            plt.show()
+            B= np.sqrt(P.B[0]**2+P.B[1]**2)#np.linalg.norm(self.B)
+            plt.scatter(np.log(d[m,:,:].reshape(-1)),np.log(B))
+            plt.title("Distance vs B")
             np.savetxt("BPhi.csv",P.phi[:,:,:].reshape(-1),delimiter=",")
+            np.savetxt("BF.csv",np.array(P.B).reshape(-1),delimiter=",")
 
     elif str(args[4]) == 'SOR':
-        P.SOR(10**(-5),50,N,phi,rho)
+        P.SOR(10**(-5),10,N,phi,rho)
 
     plt.show()
 
